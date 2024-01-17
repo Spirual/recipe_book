84fec842f1b2a5db379f8bb959fa70cb4a28af14
Spirual <spiuaal@gmail.com>
2024-01-17 11:42:27 +0300

    Создана кастомная модель пользователя.

--- backend/users/models.py
+++ backend/users/models.py
@@ -2,5 +2,33 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+class UserRole(models.TextChoices):
+    ADMIN = 'admin'
+    MODERATOR = 'moderator'
+    USER = 'user'
+
+
 class CustomUser(AbstractUser):
-    pass
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+    email = models.EmailField(
+        'Электронная почта',
+        max_length=254,
+        unique=True,
+    )
+    username = models.CharField(
+        'Имя пользователя',
+        max_length=150,
+        unique=True,
+    )
+    first_name = models.CharField('Имя', max_length=150)
+    last_name = models.CharField('Фамилия', max_length=150)
+    password = models.CharField('Пароль', max_length=150)
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
