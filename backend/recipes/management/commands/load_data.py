14eb8fca7e5bf275708b49edeeee96eec553b1f9
Spirual <spiuaal@gmail.com>
2024-01-20 15:28:34 +0300

    Настроена загрузка данных в БД через json.

---
+++ backend/recipes/management/commands/load_data.py
@@ -0,0 +1,17 @@
+import json
+
+from django.core.management.base import BaseCommand
+
+from recipes.models import Ingredient
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        with open(r'demo_data/ingredients.json', encoding='utf-8') as jsonfile:
+            ingredients = json.load(jsonfile)
+            for ingredient in ingredients:
+                Ingredient.objects.create(
+                    name=ingredient['name'],
+                    measurement_unit=ingredient['measurement_unit'],
+                )
