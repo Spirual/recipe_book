41e682c030c9f86836c3c69c882892731ec94a60
Spirual <spiuaal@gmail.com>
2024-01-31 15:48:11 +0300

    Исправлена проблема со статикой.

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -131,7 +131,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_django/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
