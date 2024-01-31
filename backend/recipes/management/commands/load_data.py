d344a81207400667ff45ff4782268e3d6362c723
Spirual <spiuaal@gmail.com>
2024-01-31 13:55:43 +0300

    Исправлены файлы main.yml, docker-compose.yml, nginx.conf.

--- backend/recipes/management/commands/load_data.py
+++ backend/recipes/management/commands/load_data.py
@@ -2,11 +2,12 @@ import json
 
 from django.core.management.base import BaseCommand
 
-from recipes.models import Ingredient
+from recipes.models import Ingredient, Tag
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        # Загрузка ингридиентов в БД.
         with open(r'demo_data/ingredients.json', encoding='utf-8') as jsonfile:
             ingredients = json.load(jsonfile)
             for ingredient in ingredients:
@@ -14,3 +15,8 @@ class Command(BaseCommand):
                     name=ingredient['name'],
                     measurement_unit=ingredient['measurement_unit'],
                 )
+
+        # Создание тегов для рецепта.
+        Tag.objects.create(name='Завтрак', color='#FFA500', slug='breakfast')
+        Tag.objects.create(name='Обед', color='#228B22', slug='lunch ')
+        Tag.objects.create(name='Ужин', color='#483D8B', slug='dinner')
